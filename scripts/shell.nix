25c9de31c59370bc7ead1132923d057008e7ced0
Jace Palmer <me@jccpalmer.com>
2025-04-26 19:00:17 -0400

    20250426 Updates

--- scripts/shell.nix
+++
@@ -1,9 +0,0 @@
-# shell.nix
-{ pkgs ? import <nixpkgs> {} }:
-
-(pkgs.buildFHSUserEnv {
-  name = "af-ollama-plugin-env";
-  targetPkgs = pkgs: [ pkgs.ollama ];
-  runScript = "/usr/local/bin/af_ollama_plugin";
-}).env
-
